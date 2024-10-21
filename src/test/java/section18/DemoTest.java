202e473dc8e827a518832c73c84c9278dd81b199
Kundan <kkrcs.01@gmail.com>
2024-10-21 18:22:50 +0530

    GitY lec-208

---
+++ src/test/java/section18/DemoTest.java
@@ -0,0 +1,25 @@
+package section18;
+
+import org.testng.annotations.Test;
+
+public class DemoTest 
+{
+	
+	@Test
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
+	
+	@Test
+	public void m2()
+	{
+		System.out.println("m2 method");
+	}
+	
+	@Test
+	public void m3()
+	{
+		System.out.println("m3 method");
+	}
+}
